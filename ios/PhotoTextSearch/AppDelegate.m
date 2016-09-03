9e28a6bf2416b523bd66a077780c60b1ba956b13
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2016-09-03 11:09:39 +0900

    Add a configuration to install into iOS device

--- ios/PhotoTextSearch/AppDelegate.m
+++ ios/PhotoTextSearch/AppDelegate.m
@@ -19,7 +19,8 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
+  //jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"]; // real device
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"PhotoTextSearch"
                                                initialProperties:nil
